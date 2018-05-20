6febd6ee8df13c178d8e1a2ff0a95b9b90875845
ElonKou <elonkou@ktime.cc>
2018-05-20 15:39:36 +0800

    Modified gitignore

---
+++ so_linux/mysum.h
@@ -0,0 +1,3 @@
+int Sum(int a, int b);
+float Sum(float a, float b);
+int multi(int a, int b);
---
+++ so_linux/run.cpp
@@ -0,0 +1,10 @@
+#include "mysum.h"
+#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", Sum(2,3));
+	printf("%f\n", Sum(4.3f,3.2f));
+	printf("%d\n", multi(2,3));
+	return 0;
+}
